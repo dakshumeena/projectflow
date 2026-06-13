0aa7af6f8c27e3a32c879bb4cc1091da34280fe9
dakshumeena <dakshumeena698@gmail.com>
2026-06-13 11:39:32 +0530

    add backend url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://project-managment-backend-mj8j.onrender.com",
 });
 
 API.interceptors.request.use(
