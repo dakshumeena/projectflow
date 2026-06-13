82264871e89d0ce877f194f74fe923a4d242ee8f
dakshumeena <dakshumeena698@gmail.com>
2026-06-13 12:52:45 +0530

    Fix workspaceapi

--- client/src/api/workspaceApi.js
+++ client/src/api/workspaceApi.js
@@ -1,21 +1,24 @@
 import API from "./axios";
 
 export const getWorkspaces = async () => {
-  const res = await API.get("/workspaces");
+  const res = await API.get("/api/workspaces");
   return res.data;
 };
 
 export const createWorkspace = async (workspaceData) => {
-  const res = await API.post("/workspaces", workspaceData);
+  const res = await API.post("/api/workspaces", workspaceData);
   return res.data;
 };
 
 export const inviteMember = async (workspaceId, email) => {
-  const res = await API.post(`/workspaces/${workspaceId}/members`, { email });
+  const res = await API.post(
+    `/api/workspaces/${workspaceId}/members`,
+    { email }
+  );
   return res.data;
 };
 
 export const deleteWorkspace = async (workspaceId) => {
-  const res = await API.delete(`/workspaces/${workspaceId}`);
+  const res = await API.delete(`/api/workspaces/${workspaceId}`);
   return res.data;
 };
